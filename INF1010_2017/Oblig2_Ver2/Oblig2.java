d1bdd8c4c62a11a0ffeb36350efcd449843d190d
andrefh <a.finn.hansen@gmail.com>
2017-06-06 00:11:26 +0200

    Add files via upload

--- INF1010_2017/Oblig2_Ver2/Oblig2.java
+++ INF1010_2017/Oblig2_Ver2/Oblig2.java
@@ -1,8 +1,8 @@
 class Oblig2{
 	public static void main(String[] args) {
-		
 
-		Elbil mini = new Elbil("BN57", 100);
+
+		Bil mini = new Elbil("BN57", 100);
 		System.out.println(mini.getClass());
 		mini.skrivInfo();
 	}
