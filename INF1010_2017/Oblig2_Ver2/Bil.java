3a35105f9727b81c10c0aafaac7f7d6be7d8da9f
andrefh <a.finn.hansen@gmail.com>
2017-06-06 00:10:58 +0200

    Add files via upload

--- INF1010_2017/Oblig2_Ver2/Bil.java
+++ INF1010_2017/Oblig2_Ver2/Bil.java
@@ -1,7 +1,11 @@
-class Bil {
+class Bil implements skrivInfo{
 	protected String skilt;
 
 	Bil(String skilt){
 		this.skilt = skilt;
+	}
+
+	public void skrivInfo(){
+		System.out.println("Nummer: " +skilt);
 	}
 }
